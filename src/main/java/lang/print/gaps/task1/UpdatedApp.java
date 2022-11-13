81dc3117cda3fe204a766b30c976f566f0b14af7
elmiguel0 <miguelangel.ramos@ibm.com>
2022-11-13 08:21:08 -0600

    print -> println on all fail test

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.print("I'm updated\n" +
+        System.out.println("I'm updated\n" +
                 "can be compiled. And can write to console");
     }
 }
